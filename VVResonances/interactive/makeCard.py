a8969fc2a6b7d5a61e7227d39f6235e2abba1b62
Simon Regnard <simon.regnard@cern.ch>
2020-05-24 07:37:10 +0200

    add syst. uncertainty for pileup from shifting the min-bias cross section

--- VVResonances/interactive/makeCard.py
+++ VVResonances/interactive/makeCard.py
@@ -101,6 +101,9 @@ for lepton in ['e','mu']:
             ## PDF
             card.addSystematic("CMS_pdf","lnN",{'XWW':1.01,'XWZ':1.01,'XWH':1.01,'VBFXWW':1.01})
 
+            ## pileup reweighting (from shifting the min-bias cross section)
+            card.addSystematic("CMS_puWeight_"+YEAR,"lnN",{'XWW':1.015,'XWZ':1.015,'XWH':1.015,'VBFXWW':1.015})
+
             ## lepton efficiency
             card.addSystematic("CMS_eff_"+lepton+"_"+YEAR,"lnN",{'XWW':1.05,'XWZ':1.05,'XWH':1.05,'VBFXWW':1.05})
 
